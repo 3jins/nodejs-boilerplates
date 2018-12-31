d9e940a58fb8cae78ff17b314e83cb48b02d68e4
Sejin Jeon <jinaidy93@gmail.com>
2018-12-31 14:36:52 +0900

    Remove router
    The router of express isn't necessary. As React is not belonging only to
    the client side, it isn't a bad idea to hand over the routing to
    React-router. This choice also can solve the problem about sharing of the
    store.

--- express-react-redux-ssr-mongodb/src/server.js
+++ express-react-redux-ssr-mongodb/src/server.js
@@ -3,13 +3,20 @@ import express from 'express';
 import createError from 'http-errors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import router from './router';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 import exampleDataAPI from './api/example-data-api';
+import reducers from './reducers';
+import renderHTML from './render-html';
+import App from './components/App';
 
 const server = express();
-
 const httpPort = process.env.NODE_ENV === 'production' ? 80 : 3000;
 const distPath = path.resolve(__dirname, '..', 'dist');
+const store = createStore(reducers, applyMiddleware(ReduxThunk));
 
 /* logging */
 server.use(logger(
@@ -22,7 +29,14 @@ server.use(cookieParser());
 /* server main logic */
 server.use(express.static(distPath));
 server.get('/example-data', exampleDataAPI);
-server.use('/', router);
+server.use('/', (() => {
+  const component = ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  return renderHTML(component);
+})()); // TODO(3jin): Find a better way than using immediately-invoked functions
 
 /* error handling */
 server.use((req, res, next) => {
